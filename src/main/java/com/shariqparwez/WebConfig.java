dc443e0f4103316cc0fca65abb16614f19b8a4e8
shariqparwez <shariqparwez@outlook.com>
2019-01-31 22:10:46 +0530

    Configuring Controller with web.xml

--- src/main/java/com/shariqparwez/WebConfig.java
+++ src/main/java/com/shariqparwez/WebConfig.java
@@ -1,10 +1,12 @@
 package com.shariqparwez;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
+@ComponentScan(basePackages = "com.shariqparwez")
 public class WebConfig {
 
 }
--- src/main/java/com/shariqparwez/controller/HelloController.java
+++ src/main/java/com/shariqparwez/controller/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
 	@RequestMapping(value = "/greeting")
 	public String sayHello(Model model) {
 		model.addAttribute("greeting", "Hello World");
-		return "hello";
+		return "hello.jsp";
 	}
 
 }
