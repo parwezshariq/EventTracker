64cfc6b23508f7c66e4d80ad19f7824b4a6b3e81
shariqparwez <shariqparwez@outlook.com>
2019-02-01 17:41:44 +0530

    POST method 405 error fix

--- src/main/java/com/shariqparwez/controller/EventController.java
+++ src/main/java/com/shariqparwez/controller/EventController.java
@@ -2,6 +2,7 @@ package com.shariqparwez.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,6 +16,12 @@ public class EventController {
 		Event event = new Event();
 		event.setName("Java User Group");
 		model.addAttribute("event", event);
+		return "event";
+	}
+	
+	@RequestMapping(value="/event", method = RequestMethod.POST)
+	public String processEvent(@ModelAttribute("event") Event event) {
+		System.out.println(event);
 		return "event";
 	}
 }
